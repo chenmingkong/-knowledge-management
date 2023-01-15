1f13e5769b68cd05f05887d2873fae1816eec3f3
chenmingkong <1924832820@qq.com>
2023-01-15 21:49:24 +0800

    初始化代码

---
+++ docs/.vuepress/config/sidebar/shared.ts
@@ -0,0 +1,14 @@
+// import fs from 'fs'
+// import path from 'path'
+//
+//
+// export function getAllFileNameArray(
+//     filePath: string,
+//     file: string
+// ) {
+//     return fs
+//         .readdirSync(path.resolve(__dirname, filePath))
+//         .map(filename => file + filename.slice(0, -3))
+//         .sort()
+// }
+
