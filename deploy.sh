7fa106aee54735f81698645f579e10dc76a1e7f2
chenmingkong <35141929+chenmingkong@users.noreply.github.com>
2023-04-29 16:42:53 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,11 +15,6 @@ cd docs/.vuepress/dist
 
 # git变更及推送
 
-git init
-git add -A
-git commit -m 'deploy'
-git config http.sslVerify "false"
-
-git push -f https://github.com/chenmingkong/chenmingkong.github.io.git master
+ls -a
 
 cd -
